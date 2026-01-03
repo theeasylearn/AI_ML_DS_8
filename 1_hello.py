1d1481c1c98b09a47cf2017efca9fd3aeaac54fe
theeasylearn <theeasylearn@gmail.com>
2026-01-03 08:08:54 +0530

    first commit

---
+++ 1_hello.py
@@ -0,0 +1,7 @@
+print('hello, this is my 1st offline python program')
+print("learning python is fun and easy")
+print("""
+one
+two
+three
+""")
---
+++ 2_input_process_output.py
@@ -0,0 +1,16 @@
+#write a program to calculate and display area of room using given length and width 
+#create variables
+length = 0 
+width = 0 
+#input 
+#variablename = input("message")
+length = input("Enter length")
+width = input("Enter width")
+#convert input into integer 
+length = int(length) 
+width = int(width)
+#process 
+# variablename = variablename operator(symbol + - * /) variablename 
+area = length * width 
+print(area)
+
