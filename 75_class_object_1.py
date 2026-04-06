6b3bf9b6c650c3d143825ec340a77d503fc8ce95
theeasylearn <theeasylearn@gmail.com>
2026-04-06 09:45:40 +0530

    OOP

---
+++ 75_class_object_1.py
@@ -0,0 +1,21 @@
+#let use create home class 
+class Home:
+    #only methods
+    def father(self):
+        print("Father will do work and earn money")
+    def mother(self):
+        print("Mother will do house hold work and take care of childrens ")
+    def child(self):
+        print("child will do play and study")
+
+#create variable of class type(object)
+#here patel is object
+patel = Home()
+patel.father()
+patel.mother()
+patel.child()
+
+dave = Home()
+dave.father()
+dave.mother()
+dave.child()
