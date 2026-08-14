d5d403ee06367d2c9606f49c4178686f29a843d5
theeasylearn <theeasylearn@gmail.com>
2026-08-14 11:15:25 +0530

    co-variance

--- MyMath.py
+++ MyMath.py
@@ -24,3 +24,18 @@ def getVariance(list,average):
     sample_variable = round(sample_variable,2)
     # print(square,total,population_variance,sample_variable)
     return population_variance,sample_variable
+
+def getCovariance(list1, list2):
+    mean1 = getMean(list1)
+    mean2 = getMean(list2)
+    total = 0
+
+    for i in range(len(list1)):
+        deviation1 = list1[i] - mean1
+        deviation2 = list2[i] - mean2
+        total = total + (deviation1 * deviation2)
+
+    population_covariance = total / len(list1)
+    sample_covariance = total / (len(list1) - 1)
+
+    return round(population_covariance, 2), round(sample_covariance, 2)
